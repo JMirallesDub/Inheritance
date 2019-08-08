4422cd2d3e75b45e0b1177dc677ad08cbe5b34bd
Jose Miralles <jemiralleslopez@gmail.com>
2019-08-08 16:20:18 +0100

    Second commit - Moving Rectangle class from Shape class

---
+++ Inheritance/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    public class Rectangle : Shape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+
+        public Rectangle(double sideA, double sideB)
+        {
+            SideA = sideA;
+            SideB = sideB;
+        }        
+        public override double Area()
+        {
+            return SideA * SideB;
+        }
+    }
+}
--- Inheritance/Shape.cs
+++ Inheritance/Shape.cs
@@ -12,20 +12,4 @@ namespace Inheritance
         public abstract double Area();
 
     }
-
-    public class Rectangle : Shape
-    {
-        public double SideA { get; }
-        public double SideB { get; }
-
-        public Rectangle(double sideA, double sideB)
-        {
-            SideA = sideA;
-            SideB = sideB;
-        }        
-        public override double Area()
-        {
-            return SideA * SideB;
-        }
-    }
 }
